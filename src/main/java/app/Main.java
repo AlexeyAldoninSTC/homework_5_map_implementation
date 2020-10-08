143783a84918b582fc8b1a142b6fdabc2d720650
Ivan31_r <ivan.usov2011@uandex.ru>
2020-10-08 13:05:57 +0300

    first commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -5,15 +5,16 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Map<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < 10; i++) {
-            map.put(i, null);
-            map.put(null, i);
-        }
-        System.out.println("map.size() = " + map.size());
-        System.out.println("map.get(10) = " + map.get(10));
-        System.out.println("map.get(null) = " + map.get(null));
-        System.out.println("map.keySet() = " + map.keySet());
-        System.out.println("map.values() = " + map.values());
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for (int i = 0; i < 10; i++) {
+//            map.put(i, null);
+//            map.put(null, i);
+//        }
+//        System.out.println("map.size() = " + map.size());
+//        System.out.println("map.get(10) = " + map.get(10));
+//        System.out.println("map.get(null) = " + map.get(null));
+//        System.out.println("map.keySet() = " + map.keySet());
+//        System.out.println("map.values() = " + map.values());
+        System.out.println("Hello, Alex");
     }
 }
