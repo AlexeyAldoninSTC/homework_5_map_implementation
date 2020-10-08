c755d7779fbb54194be60dd98649689b420d2d42
Ivan31_r <ivan.usov2011@uandex.ru>
2020-10-08 13:28:51 +0300

    Attempt 3

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -17,5 +17,6 @@ public class Main {
 //        System.out.println("map.values() = " + map.values());
         System.out.println("Hello, Alex");
         System.out.println("Helper");
+        System.out.println("Attempt 3");
     }
 }
